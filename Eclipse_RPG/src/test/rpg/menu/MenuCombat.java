3e5ee6e7075afc3058817d0a3f815e64b90bc423
Etienne DEBARD <etienne.debard@etu.univ-lyon1.fr>
2016-01-13 16:55:28 +0100

    menu combat maj

--- Eclipse_RPG/src/test/rpg/menu/MenuCombat.java
+++ Eclipse_RPG/src/test/rpg/menu/MenuCombat.java
@@ -1,26 +1,39 @@
 package test.rpg.menu;
 
+import java.util.Iterator;
 import java.util.List;
 import java.util.Random;
 
 import test.rpg.engine.Game;
+import test.rpg.engine.console.event.Command;
 import test.rpg.engine.console.event.KeyObserver;
-import test.rpg.engine.console.printer.Log;
 import test.rpg.engine.console.printer.PrintColor;
 import test.rpg.engine.interfaces.Menu;
 import test.rpg.engine.story.event.EventObserver;
 import test.rpg.perso.Entity;
 import test.rpg.perso.Personnage;
+import test.rpg.perso.competence.Capacite;
+import test.rpg.perso.equipement.Consommable;
+import test.rpg.perso.equipement.Item;
 
 public class MenuCombat extends Menu
 {
 	enum State {intro, yourTurn, enemyTurn, fin, distributeXp};
+	enum TurnState {chooseAction, listCommands, recapAction}
+	enum ActionState {listCapa, listItems}
+	
 	private int tour;
 	private int persoTurn;
 	
 	private List<Personnage> confrerie;
 	private List<Entity> monstres;
+	
 	private State state;
+	private TurnState turnState;
+	private ActionState actionState;
+	
+	private Personnage perso;
+	private Capacite capacite;
 	
 	private Random rand;
 	
@@ -51,16 +64,133 @@ public class MenuCombat extends Menu
 	
 	private void setYourTurn()
 	{
-		KeyObserver key = new KeyObserver();
-		key.addObserver(new EventObserver(){
+		if(turnState == TurnState.chooseAction)
+		{
+			
+		}
+		else
+		if(turnState == TurnState.listCommands)
+		{
+			setPersoTurn();
+		}
+		
+	}
+	
+	private void setPersoTurn()
+	{
+		perso = confrerie.get(persoTurn);
+		
+		if(actionState == ActionState.listCapa)
+		{
+			setCapaPerso();
+		}
+		else if(actionState == ActionState.listItems)
+		{
+			setConsoPerso();
+		}
+		else
+		{
+			KeyObserver key = new KeyObserver();
+			key.addObserver(new EventObserver(){
 
-			@Override
-			public void actionPerformed()
-			{
-				state = State.fin;
-			}
-		});
-		this.addCommand(key);
+				@Override
+				public void actionPerformed()
+				{
+					persoTurn++;
+					if(persoTurn >= confrerie.size())
+						state = State.enemyTurn;
+				}
+			});
+			this.addCommand(key);
+		}
+	}
+	
+	private void setCapaPerso()
+	{
+		int n = 1;
+		Iterator<Consommable> i = perso.getClasse().getCapa().iterator();
+		while(i.hasNext())
+		{
+			Consommable conso = i.next();
+			Command comp = new Command("Capacite : " + conso.toString(), ""+ n);
+			comp.addObserver(new EventObserver(){
+				@Override
+				public void actionPerformed()
+				{
+					setCapacite(capa);
+				}
+			});
+			
+			this.addCommand(comp);
+			n++;
+		}
+		
+		if(n == 1)
+		{
+			KeyObserver key = new KeyObserver();
+			key.addObserver(new EventObserver(){
+
+				@Override
+				public void actionPerformed()
+				{
+				}
+			});
+			this.addCommand(key);
+		}
+	}
+	
+	private void setConsoPerso()
+	{
+		int n = 1;
+		Iterator<Capacite> i = perso.getClasse().getCapa().iterator();
+		while(i.hasNext())
+		{
+			Capacite capa = i.next();
+			Command comp = new Command("Capacite : " + capa.toString(), ""+ n);
+			comp.addObserver(new EventObserver(){
+				@Override
+				public void actionPerformed()
+				{
+					setCapacite(capa);
+				}
+			});
+			
+			this.addCommand(comp);
+			n++;
+		}
+		
+		if(n == 1)
+		{
+			KeyObserver key = new KeyObserver();
+			key.addObserver(new EventObserver(){
+
+				@Override
+				public void actionPerformed()
+				{
+				}
+			});
+			this.addCommand(key);
+		}
+	}
+	
+	public void setCapacite(Capacite capa)
+	{
+		this.capacite = capa;
+	}
+	
+	public void setConsommable(Consommable conso)
+	{
+		
+	}
+	
+	private void executeCapacite()
+	{
+		
+	}
+	
+	private void useConsommable()
+	{
+		
 	}
 	
 	private void setEnemyTurn()
@@ -109,7 +239,7 @@ public class MenuCombat extends Menu
 
 	private void renderYourTurn()
 	{
-		writeLine("c'est a votre tour", PrintColor.CYAN);
+		writeLine("C'est au tour de " + perso.getNom() + " d'attaquer.", PrintColor.CYAN);
 	}
 
 	private void renderIntro()
