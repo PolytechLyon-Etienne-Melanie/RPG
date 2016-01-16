f9eb2008ed216782168a3830aaeb79305fb46d7a
PETITCUENOT Mélanie <melanie.petitcuenot@etu.univ-lyon1.fr>
2016-01-16 21:01:33 +0100

    16/01
    Début diapo (vraiment le début du début);
    histoire voleur;
    modif inventaire;
    histoire

--- Eclipse_RPG/src/test/rpg/menu/MenuInventaire.java
+++ Eclipse_RPG/src/test/rpg/menu/MenuInventaire.java
@@ -3,13 +3,19 @@ package test.rpg.menu;
 import java.util.Iterator;
 
 import test.rpg.engine.Game;
+import test.rpg.engine.console.event.Command;
 import test.rpg.engine.interfaces.Menu;
+import test.rpg.engine.story.event.EventObserver;
 import test.rpg.perso.equipement.Inventaire;
 import test.rpg.perso.equipement.Item;
 
 public class MenuInventaire extends Menu{
 	
 	private Inventaire inventaire;
+	private Command retour;
+	private Command equipeArme;
+	private Command equipeArmure;
+	private Command utiliserPotion;
 	
 	public MenuInventaire(Game game, Inventaire invent )
 	{
@@ -20,7 +26,46 @@ public class MenuInventaire extends Menu{
 	}
 
 	protected void setCommands(){
+		retour = new Command("Retourner au jeu.", "exit");
+		retour.addObserver(new EventObserver(){
+			@Override
+			public void actionPerformed()
+			{
+				game.returnToStory();
+			}
+		});
 		
+		equipeArme = new Command("Voulez-vous équiper une arme ?", "Arme");
+		equipeArme.addObserver(new EventObserver(){
+			@Override
+			public void actionPerformed()
+			{
+				//comment on gère le choix de l'item à équiper ?
+			}
+		});
+		
+		equipeArmure = new Command("Voulez-vous équiper une armure ?", "Armure");
+		equipeArmure.addObserver(new EventObserver(){
+			@Override
+			public void actionPerformed()
+			{
+				//à compléter
+			}
+		});
+		
+		utiliserPotion = new Command("Voulez-vous utiliser une potion ?", "Potion");
+		utiliserPotion.addObserver(new EventObserver(){
+			@Override
+			public void actionPerformed()
+			{
+				//à compléter
+			}
+		});
+		
+		this.addCommand(retour);
+		this.addCommand(equipeArme);
+		this.addCommand(equipeArmure);
+		this.addCommand(utiliserPotion);
 	}
 	
 	
@@ -32,13 +77,14 @@ public class MenuInventaire extends Menu{
 
 	@Override
 	protected void renderMenu() {
-		writeLine("_________________________________________________________________________________________________________________________________________________________");
-		writeLine("  _|      _|              _|                              _|_|_|                                              _|                _|                      ");         
-		writeLine("  _|      _|    _|_|    _|_|_|_|  _|  _|_|    _|_|          _|    _|_|_|    _|      _|    _|_|    _|_|_|    _|_|_|_|    _|_|_|      _|  _|_|    _|_|   ");          
-		writeLine("  _|      _|  _|    _|    _|      _|_|      _|_|_|_|        _|    _|    _|  _|      _|  _|_|_|_|  _|    _|    _|      _|    _|  _|  _|_|      _|_|_|_|  "); 
-		writeLine("    _|  _|    _|    _|    _|      _|        _|              _|    _|    _|    _|  _|    _|        _|    _|    _|      _|    _|  _|  _|        _|         "); 
-		writeLine("       _|        _|_|        _|_|  _|          _|_|_|      _|_|_|  _|    _|      _|        _|_|_|  _|    _|      _|_|    _|_|_|  _|  _|          _|_|_|  ");
-		writeLine("_________________________________________________________________________________________________________________________________________________________");
+		writeLine(" _____________________________________________________________________________________");
+		writeLine("| __      __   _              _____                      _        _                  |");         
+		writeLine("| \\ \\    / /  | |            |_   _|                    | |      (_)             _   |");          
+		writeLine("|  \\ \\  / /__ | |_ _ __ ___    | |  _ ____   _____ _ __ | |_ __ _ _ _ __ ___    (_)  |"); 
+		writeLine("|   \\ \\/ / _ \\| __| '__/ _ \\   | | | '_ \\ \\ / / _ \\ '_ \\| __/ _` | | '__/ _ \\        |"); 
+		writeLine("|    \\  / (_) | |_| | |  __/  _| |_| | | \\ V /  __/ | | | || (_| | | | |  __/    _   |");
+		writeLine("|     \\/ \\___/ \\__|_|  \\___| |_____|_| |_|\\_/ \\___|_| |_|\\__\\__,_|_|_|  \\___|   (_)  |");
+		writeLine("|____________________________________________________________________________________|");
 		writeLine("");
 		
 		writeLine("Vous possedez les items suivant : ");
@@ -55,6 +101,6 @@ public class MenuInventaire extends Menu{
 		
 		writeLine("");
 		writeLine("Vous êtes équipé de l'armure suivante :" +inventaire.getArmureeq());
-	} //commande sorti + regler pb armure + refaire titre (pas beau) + command equiper/jeter/ ...
+	}	// command equiper/jeter/ ...
 
 }
